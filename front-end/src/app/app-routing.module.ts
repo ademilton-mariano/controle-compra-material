32945f50c617f0931b7633b468dd5446b9cd0e67
Ademilton Mariano <ademilton.mariano@gmail.com>
2024-01-27 20:38:06 -0300

    Criando pagina inicial

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SolicitanteComponent } from './solicitante/solicitante.component';
+import { AprovadorComponent } from './aprovador/aprovador.component';
+import { AdministrativoComponent } from './administrativo/administrativo.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/pagina-inicial', pathMatch: 'full' },
+  { path: 'pagina-inicial', component: PaginaInicialComponent },
+  { path: 'solicitante', component: SolicitanteComponent },
+  { path: 'aprovador', component: AprovadorComponent },
+  { path: 'administrativo', component: AdministrativoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
